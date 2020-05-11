66f65af47f3fc0a01bf74dfc0ac8822aaa9d0af1
nicbongo <nicbongo@protonmail.com>
2020-05-11 16:59:50 -0300

    Stack: Tests passed

--- stack/stack.py
+++ stack/stack.py
@@ -19,16 +19,23 @@ class Stack:
         return len(self.storage)
 
     def push(self, value):
+        self.size += value
         self.storage.append(value)
 
     def pop(self):
-        self.storage.pop()
+        if len(self.storage) < 1:
+            return None   
+        return self.storage.pop()
 
 stack = Stack()
 stack.push(2)
 stack.push(4)
+stack.push(4)
+stack.push(2)
+stack.pop()
 
 print(stack.storage)
+print(stack.size)
 
 
 
@@ -40,43 +47,43 @@ print(stack.storage)
 
 
 
-# class Node:
-#     def __init__(self, value=None, next_node = None):
-#         self.value = value
-#         self.next_node = next_node
+class Node:
+    def __init__(self, value=None, next_node = None):
+        self.value = value
+        self.next_node = next_node
         
-#     def get_value(self):
-#         return self.value
+    def get_value(self):
+        return self.value
     
-#     def get_next(self):
-#         return self.next_node
+    def get_next(self):
+        return self.next_node
     
-#     def set_next(self, new_next):
-#         self.next_node = new_next
+    def set_next(self, new_next):
+        self.next_node = new_next
 
-# class LinkedList:
-#     def __init__(self, head=None):
-#         # first node in list
-#         self.head = head
+class LinkedList:
+    def __init__(self, head=None):
+        # first node in list
+        self.head = head
         
-#     def add(self, value):
-#         #regardless, need to wrap value in new node
-#         new_node = Node(value)
+    def add(self, value):
+        #regardless, need to wrap value in new node
+        new_node = Node(value)
         
-#         # what if the list is empty
-#         if not self.head:
-#             self.head = new_node
+        # what if the list is empty
+        if not self.head:
+            self.head = new_node
         
-#         # what if list is not empty
-#         else:
-#             # set the new node as next node
-#             current = self.head
-#             while current.get_next() is not None:
-#                 current = current.get_next()
-#             current.set_next(new_node)
+        # what if list is not empty
+        else:
+            # set the new node as next node
+            current = self.head
+            while current.get_next() is not None:
+                current = current.get_next()
+            current.set_next(new_node)
         
 
-# ll = Node(1)
-# ll.next_node = Node(2)
-# ll.next_node.next_node = Node(3)
-# ll.next_node.next_node.next_node = Node(4)
+ll = Node(1)
+ll.next_node = Node(2)
+ll.next_node.next_node = Node(3)
+ll.next_node.next_node.next_node = Node(4)
